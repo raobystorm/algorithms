a61e432a4e647dda0c7c6b600cf7357f48992139
raobystorm <raobystorm@gmail.com>
2015-03-13 22:01:43 +0900

    Uninish

---
+++ LCOJ_140/LCOJ_140.cpp
@@ -0,0 +1,32 @@
+// LCOJ_140.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <fstream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<string> wordBreak(string s, unordered_set<string> &dict) {
+
+	}
+};
+
+int main()
+{
+	ifstream fin("in.txt");
+	ofstream fout("out.txt");
+	
+	string s;
+	unordered_set<string> dict();
+
+	Solution s;
+	vector<string> sents;
+
+	return 0;
+}
+
