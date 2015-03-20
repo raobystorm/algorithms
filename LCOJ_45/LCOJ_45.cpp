f16c617b0a74cec139674706c228e90069ee2e01
raobystorm <raobystorm@gmail.com>
2015-03-20 16:23:50 +0900

    AC

--- LCOJ_45/LCOJ_45.cpp
+++ LCOJ_45/LCOJ_45.cpp
@@ -6,24 +6,17 @@
 
 int jump(int A[], int n) {
 
-	if (n <= 1) return 0;
-	int idx = 0;
-	int max[1000] = {0}, maxidx; // max distance of the i+1 steps could reach
-	bool noloop = true;
-	max[0] = A[0]> 1 ? A[0]: 1;
-	if (max[0] >= n - 1) return 1;
+	if (n == 0) return 0;
 
-	for (int i = 1; i < n; i++){
-		int j = 0; // how many steps need to be here
-		for (; j < 1000 && max[j] < i; j++);
-
-		if (!j) j++;
-
-		if (A[i] + j >= n - 1) return j + 1;
-
-		if (A[i] + j > max[j]) { 
-			
-			max[j] = A[i] + j; 
+	int preMax = A[0], max = A[0], step = 1;
+	for (int i = 1; i <= n - 1 && i <= preMax; i++){
+		
+		max = A[i] + i > max ? A[i] + i : max;
+		if (i == n - 1)return step;
+		if (i == preMax){
+		
+			preMax = max;
+			step++;
 		}
 	}
 	return 0;
@@ -31,8 +24,9 @@ int jump(int A[], int n) {
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int A[4] = { 1, 1, 1, 1};
-	int res = jump(A, 4);
+	int A[5] = { 1, 2, 3, 4, 5};
+	int res = jump(A, 5);
+
 	return 0;
 }
 
