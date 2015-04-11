2b038d659f570b09a4416c9f3793dfcf6f7b2f4a
raobystorm <raobystorm@gmail.com>
2015-04-11 19:12:12 +0900

    AC

--- LCOJ_200/LCOJ_200.c
+++ LCOJ_200/LCOJ_200.c
@@ -1,39 +1,39 @@
 
 void dfs(char** grid, int row, int column, int numRows, int numColumns){
 
-	if(grid[row][column] <= 0) return;
+	if (grid[row][column] == '0' || grid[row][column] == -1) return;
 
 	grid[row][column] = -1;
 
-	if(row > 0) dfs(grid, row - 1, column, numRows, numColumns);
-	if(row < numRows - 1) dfs(grid, row + 1, column, numRows, numColumns);
-	if(column > 0) dfs(grid, row, column - 1, numRows, numColumns);
-	if(column < numColumns - 1) dfs(grid, row, column + 1, numRows, numColumns);
+	if (row > 0) dfs(grid, row - 1, column, numRows, numColumns);
+	if (row < numRows - 1) dfs(grid, row + 1, column, numRows, numColumns);
+	if (column > 0) dfs(grid, row, column - 1, numRows, numColumns);
+	if (column < numColumns - 1) dfs(grid, row, column + 1, numRows, numColumns);
 }
 
 int numIslands(char **grid, int numRows, int numColumns) {
-    
-    if(!numRows || !numColumns) return 0;
 
-    int n = 0;
+	if (!numRows || !numColumns) return 0;
 
-    for(int i = 0; i < numRows; i++){
+	int n = 0;
 
-    	for(int j = 0; j < numColumns; j++){
+	for (int i = 0; i < numRows; i++){
 
-    		if(grid[i][j] == -1) continue;
-    		else if(grid[i][j] == 0) {
+		for (int j = 0; j < numColumns; j++){
 
-    			grid[i][j] = -1;
-    			continue;
-    		}
-    		else{
+			if (grid[i][j] == -1) continue;
+			else if (grid[i][j] == '0') {
 
-    			n++;
-    			dfs(grid, i, j, numRows, numColumns);    			
-    		}
-    	}
-    }
+				grid[i][j] = -1;
+				continue;
+			}
+			else{
 
-    return n;
+				n++;
+				dfs(grid, i, j, numRows, numColumns);
+			}
+		}
+	}
+
+	return n;
 }
