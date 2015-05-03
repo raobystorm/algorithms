c51be38e63eedb68552829032a7dcedfc47bbf05
raobystorm <raobystorm@gmail.com>
2015-05-03 22:02:22 +0100

    AC

---
+++ LCOJ_169/LCOJ_169.cpp
@@ -0,0 +1,19 @@
+#define MAX 100000
+
+int majorityElement(int* nums, int numsSize)
+{
+  if(numSize <= 1) return *nums;
+  
+  int count[
+  unordered_map<int, int> count;
+  for (int i = 0; i < numSize; i++) {
+    count[nums[i]]++;
+    if(count[nums[i]] > (n-1)/2) return nums[i];
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  
+  return 0;
+}
--- hiOJ_1136/hiOJ_1136.cpp
+++ hiOJ_1136/hiOJ_1136.cpp
@@ -9,46 +9,13 @@ using namespace std;
 
 #define MAX 100000
 
-struct module
-{
-  int input;
-  int count;
-  list<int> outputs;
-  // Use hash to map the signal into the output list of modules
-  unordered_map< int, list<module*> >* modules;
-  module(): input(0), count(0), outputs(NULL){};
-  // DFS search on the modules when get inputs
-  void activate()
-  {
-    if(outputs.size() == 0) return;
-
-    list<int>::iterator itr = outputs.begin();
-
-    while (itr != outputs.end()) {
-
-      if(modules->find(*itr) != modules->end())
-      {
-        list<module*>::iterator litr = (*modules)[*itr].begin();
-        while(litr != (*modules)[*itr].end())
-        {
-          (*litr)->count += 1;
-          (*litr)->activate();
-        }
-      }
-        
-      itr++;
-    }
-  }
-};
-
 int main() {
 
   int t, m, n, tmpin, s;
   queue<int> signals;
-  unordered_map<int, list<module*>> modules;
-  unordered_map<int, list<module*>>::iterator mitr;
-  
-  
+  int count[MAX] = {0};
+  unordered_map<int, list<int>*> modules;
+
   scanf("%d", &t);
   // Loop for the cases
   for( int i = 0; i < t; i++)
@@ -56,8 +23,6 @@ int main() {
     scanf("%d%d", &n, &m);
 
     signals = queue<int>();
-    modules = unordered_map<int, list<module*>>();
-    mitr = modules.begin();
     // Loop for reading initial signals
     for( int j = 0; j < m; j++)
     {
@@ -67,45 +32,29 @@ int main() {
     // Loop for setting up modules
     for( int j = 0; j < n; j++)
     {
+      count[j] = 0;
       scanf("%d", &tmpin);
-      if(modules.find(tmpin) == modules.end())
-      {
-        // This module has not been added into the map
-        module tmpModule;
-        tmpModule.input = tmpin;
-        tmpModule.count = 0;
-        tmpModule.activate();
+      list<int> outputs;
+      int k;
+      scanf("%d", &k);
+      for (int l = 0; l < k ; l++) {
         
-        list<module*> newList;
-        
-        
-      modules[j].count = 0;
-      modules[j].input = tmpin;
-      scanf("%d", &tmpin);
-      modules[j].outputs = list<module*>();
-      
-      for( int k = 0; k < tmpin; k++)
-      {
-        if(mitr = modules.find(tmpin) == modules.end())
-        {
-          list<module*> tmplist;
-          tmplist.insert(modules
+        int tmp;
+        scanf("%d",&tmp);
+        outputs.insert(tmp);
       }
-    }
+
+      modules[tmpin] =  &outputs;
+    }    
     
     while (!signals.empty()) 
     {
       tmpin = signals.front();
       signals.pop();
       
-      for (int j = 0; j < n; j++)
-        if(modules[j].input == tmpin)
-        {
-          modules[j].count++;
-          for (int k = 0; k < modules[j].outputs.size(); k++) {
-            signals.push(modules[j].outputs[k]);
-          }
-        }
+      if(modulus.find(tmpin) != modules.end())
+      {
+        
     }
 
     for( int j = 0; j < n; j++)
