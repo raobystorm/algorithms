bd195b8d51d3818b631d1884e63771e578bbef67
raobystorm <raobystorm@gmail.com>
2015-06-16 06:44:08 +0900

    A implementation for array combinations

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -2,8 +2,10 @@
 //
 
 #include "stdafx.h"
-#include "string"
-#include "iostream"
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -22,21 +24,60 @@ void StringPermute(std::string& s, int i){
 	}
 }
 
+// a method for all combinations without duplicates
+template<typename _iter>
+void combination(_iter _array, int len){
+
+	if (len < 2) return;
+
+	bool end = false;
+	while (true){
+
+		for (int i = 0; i < len; i++)
+			cout << _array[i] << " ";
+		cout << endl;
+
+		int i, j;
+		for (i = len - 2; i >= 0; i--){
+			if (_array[i] < _array[i + 1]) break;
+			else if (i == 0) return;
+		}
+
+		for (j = len - 1; j > i; j--){
+			if (_array[j] > _array[i])break;
+		}
+		swap(_array[i], _array[j]);
+		for (int k = i + 1, l = len - 1; k < l; k++, l--)
+			swap(_array[k], _array[l]);
+	}
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::string ss;
+	vector<int> vec;
+	int n, tmp;
 
 	while (1){
 	
 		_wsystem(L"cls");
 
-		std::cout << "Please input the string to permute:\n";
+		/*std::cout << "Please input the string to permute:\n";
 
 		std::cin >> ss;
 
-		if (ss == "exit") return 0;
+		if (ss == "exit") return 0;*/
 
-		StringPermute(ss, 0);
+		cin >> n;
+		vec.clear();
+		for (int i = 0; i < n; i++){
+			
+			cin >> tmp;
+			vec.push_back(tmp);
+		}
+
+		sort(vec.begin(), vec.end());
+		combination(vec, n);
 
 		_wsystem(L"pause");
 	}
