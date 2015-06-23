ed028854879c7de45e6172ffeeca61a480565eec
raobystorm <raobystorm@gmail.com>
2015-06-23 20:43:43 +0900

    Manacher added

--- LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
+++ LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
@@ -9,6 +9,10 @@
 
 using namespace std;
 
+inline int min(const int& a, const int& b){
+	return a > b ? b : a;
+}
+
 inline bool isPalindrom(string& s){
 	for (int i = 0; i < s.size()/2; i++){
 		if (s[i] != s[s.size() - 1 - i]) return false;
@@ -17,7 +21,7 @@ inline bool isPalindrom(string& s){
 }
 
 int BruteSearch(string& s){
-
+	// O(n^3) brute search
 	int res = 0;
 	for (int i = 2; i <= s.size(); i++){
 		// i is the length of the substr
@@ -34,6 +38,7 @@ int BruteSearch(string& s){
 int DynamicProgramming(string& s){
 
 	// dp[j][i] is whether substr[j...i] a palindrom substr
+	// O(n^2) time & space
 	vector<vector<bool> > dp(s.size(), vector<bool>(s.size(), true));
 	int count = 0;
 	for (int i = 0; i < s.size(); i++)
@@ -51,6 +56,68 @@ int DynamicProgramming(string& s){
 	return count;
 }
 
+int Stretch(string& s){
+	// O(n^2) time
+	int left, right, count = 0, tmp = 0;
+	for (int i = 1; i < s.size(); i++){
+		// for every elem, assume they are center of the palidrom substr
+		// try to search their neighbours if this assumption is true
+		left = i - 1;
+		right = i;
+		tmp = 0;
+		// the situation of even length
+		while (left >= 0 && right < s.size() && s[left] == s[right]){
+
+			tmp = right - left + 1;
+			right++;
+			left--;
+		}
+		count = count > tmp ? count : tmp;
+
+		left = i - 1;
+		right = i + 1;
+		tmp = 0;
+		// the length is odd
+		while (left >= 0 && right < s.size() && s[left] == s[right]){
+
+			tmp = right - left + 1;
+			right++;
+			left--;
+		}
+		count = count > tmp ? count : tmp;
+	}
+	return count;
+}
+
+int Manacher(string& s){
+	// Most mysterous algo for this problem. O(n)
+	// Add padding chars to unify the even and odd strs
+	string ss(s.size()*2+2, '#');
+	ss[0] = '$';
+	int i = 0, j = 2;
+	for (; i < s.size(); i++, j += 2)
+		ss[j] = s[i];
+
+	int mx = 0, id = 0, max = 0;
+	vector<int> p(ss.size()+1, 0);
+	// p[i]: length of the longest palindrom substrs that centered at ss[i].
+	// id : center location of the longest palindrom substr
+	// mx : border of the longest palindrom substr i.e. mx = id + p[id]
+	// 2 * id - i: the symmetry pos of i centering id.
+	// if mx > i, then p[i] = min(p[2 * id -i ], mx - i)
+	for (int i = 1; i <= ss.size(); i++) {
+		p[i] = mx > i ? min(p[2 * id - i], mx - i) : 1;
+		while (i+p[i] < ss.size() && i-p[i] >=0 && ss[i + p[i]] == ss[i - p[i]]) p[i]++;
+		if (i + p[i] > mx) {
+			mx = i + p[i];
+			id = i;
+		}
+	}
+
+	for (auto i : p) if (i > max) max = i;
+	return max-1;
+}
+
 int main()
 {
 	int n = 0, ans = 0; 
@@ -58,7 +125,7 @@ int main()
 	string s;
 	while (n--){
 		cin >> s;
-		cout << DynamicProgramming(s) << endl;
+		cout << Manacher(s) << endl;
 	}
 	return 0;
 }
