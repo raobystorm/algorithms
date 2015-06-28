f168b7ba9b74c287e39c614ca2856d730429033b
raobystorm <raobystorm@gmail.com>
2015-06-28 19:07:48 +0900

    Symmetric substr, Manacher

--- LCOJ_214_cpp/LCOJ_214_cpp.cpp
+++ LCOJ_214_cpp/LCOJ_214_cpp.cpp
@@ -2,7 +2,6 @@
 //
 
 #include "stdafx.h"
-#include <unordered_set>
 #include <vector>
 #include <string>
 
@@ -12,32 +11,15 @@ class Solution {
 public:
 	int min(const int& a, const int& b){ return a > b ? b : a; }
 	int max(const int& a, const int& b){ return a < b ? b : a; }
-	bool repeated(string& s){
-		unordered_set<char> set;
-		for (auto c : s){
-			if (set.find(c) == set.end()) set.insert(c);
-			else return true;
-		}
-		return false;
-	}
-	string shortestPalindrome(string s) {
-
-		if (!repeated(s)){
-			string res = s;
-			reverse(res.begin(), res.end());
-			res = res.substr(0, res.size() - 1);
-			res += s;
-			return res;
-		}
-
-		string ss(s.size() * 2 + 2, '#'), res;
+	// Using manacher to guarantee the performance
+	string manacher(string& s, vector<int>& p){
+	
+		string ss(s.size() * 2 + 2, '#');
 		ss[0] = '$';
-		int i = 0, j = 2, len;
+		int i = 0, j = 2, mx = 0, id = 0;
 		for (; i < s.size(); i++, j += 2)
 			ss[j] = s[i];
-		// Search every pos for maximum palindrome already exists
-		int mx = 0, id = 0, minLength = 999999, idx = 0;
-		vector<int> p(ss.size() + 1, 0);
+
 		for (int i = 1; i <= ss.size(); i++) {
 			p[i] = mx > i ? min(p[2 * id - i], mx - i) : 1;
 			while (i + p[i] < ss.size() && i - p[i] >= 0 && ss[i + p[i]] == ss[i - p[i]]) p[i]++;
@@ -46,57 +28,57 @@ public:
 				id = i;
 			}
 		}
-		// minLength is the minimum length for adding chars to become a palindrome
-		for (int i = 0; i < p.size(); i++){
-			if (minLength > (max(i, p.size() - 1 - i) - p[i])){
-				minLength = (max(i, p.size() - 1 - i) - p[i]);
-				idx = i;
-			}
-		}
-		// calc the size of the result string
-		len = idx > (p.size() / 2) ? idx : p.size() - idx;
-		len /= 2;
+		return ss;
+	}
+	
+	string shortestPalindrome(string s) {
 
-		if (idx < p.size() / 2){
-			res = string(len, '#');
-			j = 0;
-			for (int i = idx; i < p.size(); i++){
-				if (ss[i] != '#' && ss[i] != '$') {
-					res[j] = ss[i];
-					j++;
+		if (s.size() < 2) return s;
+		vector<int> p(s.size()*2+3, 1);
+		string ss = manacher(s, p);
+
+		string res, tmps;
+		int idx = 0, end = 0, len = 0, tmp, start;
+		bool even = false;
+		for (int i = 2; i < p.size()-1; i++){
+
+			if (p[i] - 1 > len){
+
+				end = i > (p.size() - 2)/2;
+				// check if this palindrome substr could reach the head or tail
+				if (end && (i-2)/2+(p[i]-1)/2 >= s.size()-1){
+
+					len = p[i] - 1;
+					idx = (i - 2) / 2;
+					even = ss[i] == '#';
+				}
+				else if (!end && (p[i]-1)/2 >= (i-1)/2){
+
+					len = p[i] - 1;
+					idx = (i - 2) / 2;
+					even = ss[i] == '#';
 				}
 			}
+		}
 
-			string ans = res;
-			if (ans[ans.size() - 1] == '\0') ans = ans.substr(0, ans.size() - 1);
-			reverse(ans.begin(), ans.end());
-			if (ss[idx] != '#') ans = ans.substr(0, ans.size()-1);
-			ans += res;
-			return ans;
+		end = idx >= s.size() / 2;
+		// consider the situation seprately
+		if (even){
+			if (end) { tmps = s.substr(0, 2 * idx - s.size() + 2); reverse(tmps.begin(), tmps.end()); res = s + tmps; }
+			else { tmps = s.substr(2 * idx + 2); reverse(tmps.begin(), tmps.end()); res = tmps + s; }
 		}
 		else{
-			res = string(len, '#');
-			j = 0;
-			for (int i = 0; i <= idx; i++){
-				if (ss[i] != '#' && ss[i] != '$') {
-					res[j] = ss[i];
-					j++;
-				}
-			}
-			string ans = res;
-			if (ans[ans.size() - 1] == '\0') ans = ans.substr(0, ans.size() - 1);
-			reverse(ans.begin(), ans.end());
-			if (ss[idx] != '#') ans = ans.substr(1, ans.size() - 1);
-			res += ans;
-			return res;
+
+			if (end){ tmps = s.substr(0, 2 * idx - s.size() + 1); reverse(tmps.begin(), tmps.end()); res = s + tmps; }
+			else{ tmps = s.substr(idx * 2 + 1); reverse(tmps.begin(), tmps.end()); res = tmps + s; }
 		}
-		return "";
+		return res;
 	}
 };
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	string s = Solution().shortestPalindrome("cdabbadc");
+	string s = Solution().shortestPalindrome("nllynrwlxijbpxtrtwnxxaetdtwzcbljckitulbfurcxlcnlpbp");
 	return 0;
 }
 
