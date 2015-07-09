71dfaef8acebbad7b244c2af3a6173880695de92
raobystorm <raobystorm@gmail.com>
2015-07-09 21:46:09 +0900

    change name

---
+++ hihoOJ_1081/hihoOJ_1081.cpp
@@ -0,0 +1,20 @@
+// hihoOJ_1081.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n, m, s, t;
+
+	scanf_s("%d%d%d%d", &n, &m, &s, &t);
+
+	for (int i = 0; i < m; i++){
+		
+	}
+	return 0;
+}
+
