d8440a4e7b38022638fff40b4fd521c2cd47ebbe
raobystorm <raobystorm@gmail.com>
2015-07-31 15:03:33 +0900

    ac

--- LCOJ_235_Java/src/Solution.java
+++ LCOJ_235_Java/src/Solution.java
@@ -3,11 +3,11 @@
  */
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return null;
-        if(p.val > root.val && q.val > root.val) {
+        if (root == null) return null;
+        if (p.val > root.val && q.val > root.val) {
             return lowestCommonAncestor(root.right, p, q);
-        }else if(p.val < root.val && q.val < root.val){
+        } else if (p.val < root.val && q.val < root.val) {
             return lowestCommonAncestor(root.left, p, q);
-        }else return root;
+        } else return root;
     }
 }
