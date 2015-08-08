82bbd7c1e9a8dc6ee7f7a7cc260548ba4dcd87a5
raobystorm <raobystorm@gmail.com>
2015-08-08 21:22:58 +0100

    update

--- ImageRevise_Python/ImageRevise.py
+++ ImageRevise_Python/ImageRevise.py
@@ -5,12 +5,35 @@ import xml.etree.ElementTree
 # the general name of the folder of bounding box
 bbFolderStr = "BoundingBox"
 # the size of processed image
-reSize = [224, 224]
+(width, height) = (224, 224)
 
 # this is the location of the folder contains image folders
 imgFolder = "/media/raoby/UnixExt/ILSVRC2012/train"
 # this is the location of the folder contians bounding boxes
 bbFolder = "/media/raoby/UnixExt/ILSVRC2012/BoundingBox"
+
+def singleRevise(imgLoc, xmlFileLoc):
+	# The function is used for revise single image
+	# @param {imgLoc} the location of target image
+	# @param {xmlFileLoc} the location of the bounding box of current image
+	# @return {errMsg} the results of the execution
+	xmin, xmax, ymin, ymax = 0, 0, 0, 0
+	currImg = Image.open(imgLoc);
+	if currImg is not None:
+		w, h = currImg.size
+	root = xml.etree.ElementTree.parse(xmlFileLoc).getroot()
+	desImg = Image.new(currImg.mode, (width, height))
+	if root is not None:
+		xmin = int(root.find("object").find("bndbox").find("xmin").text)
+		xmax = int(root.find("object").find("bndbox").find("xmax").text)
+		ymin = int(root.find("object").find("bndbox").find("ymin").text)
+		ymax = int(root.find("object").find("bndbox").find("ymax").text)
+	currImg = currImg.crop((xmin, h - ymax, xmax, h - ymin))
+	ratio = min(width/w, height/h)
+	currImg = currImg.resize((width*ratio, height*ratio))
+	desImg.paste(currImg, (xmin, h - ymax, xmax, h - ymin))
+	desImg.save(imgLoc, "JPEG")
+
 
 def ImageRevise(imgFolderLoc, bbFolderLoc):
 	# The function for all image pre-process
@@ -24,33 +47,9 @@ def ImageRevise(imgFolderLoc, bbFolderLoc):
 		for imgStr in imgList:
 			# processing one image of the class in {folder}
 			imgStr = imgStr[0 : imgStr.rfind(".")]
-			xmin, xmax, ymin, ymax = 0, 224, 0, 224
-			currImg = Image.open(folderLoc + "/" + imgStr + ".JPEG")
-			if currImg is not None:
-				xmax, ymax = currImg.size
+			xmin, xmax, ymin, ymax = 0, 0, 0, 0
+			currImgLoc = folderLoc + "/" + imgStr + ".JPEG"
 			xmlFileLoc = bbFolderLoc + "/" + folder + "/" + imgStr + ".xml"
-			root = xml.etree.ElementTree.parse(xmlFileLoc).getroot()
-			if root is not None:
-				xmin = int(root.find("object").find("bndbox").find("xmin").text)
-				xmax = int(root.find("object").find("bndbox").find("xmax").text)
-				ymin = int(root.find("object").find("bndbox").find("ymin").text)
-				ymax = int(root.find("object").find("bndbox").find("ymax").text)
-			
-
-def singleRevise(imgLoc, xmlFileLoc):
-	xmin, xmax, ymin, ymax = 0, 224, 0, 224
-	currImg = Image.open(imgLoc);
-	if currImg is not None:
-		w, h = currImg.size
-	root = xml.etree.ElementTree.parse(xmlFileLoc).getroot()
-	if root is not None:
-		xmin = int(root.find("object").find("bndbox").find("xmin").text)
-		xmax = int(root.find("object").find("bndbox").find("xmax").text)
-		ymin = int(root.find("object").find("bndbox").find("ymin").text)
-		ymax = int(root.find("object").find("bndbox").find("ymax").text)
-	currImg = currImg.crop((xmin, h - ymax, xmax, h - ymin))
-	currImg = currImg.resize(reSize)
-	currImg.save(imgLoc, "JPEG")
-
+			singleRevise(currImgLoc, xmlFileLoc)
 
 singleRevise( "/media/raoby/UnixExt/ILSVRC2012/train/n02132136/n02132136_16.JPEG" ,"/media/raoby/UnixExt/ILSVRC2012/BoundingBox/n02132136/n02132136_16.xml")
